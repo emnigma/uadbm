c4f55ca59dc52478188f48fb5f9e6d3ede0f2e8a
Max Nigmatulin <40598909+emnigma@users.noreply.github.com>
2024-12-18 15:19:05 +0700

    feat: Add volume & image saving option (#2)
    * add save volume option
    
    * fix wrong config name

--- recon_one.py
+++ recon_one.py
@@ -1,11 +1,4 @@
 import argparse
-
-# download_brainweb_dataset(
-#     base_dir=Path('./content/data/Brainweb'),
-#     name="",
-#     institution="",
-#     email=""
-# )
 import json
 import os
 import time
@@ -18,34 +11,10 @@ import scipy
 import tensorflow as tf
 from imageio import imwrite
 
-from dataloaders.BRAINWEB import BRAINWEB
-from models import (
-    anovaegan,
-    autoencoder,
-    constrained_adversarial_autoencoder,
-    context_encoder_variational_autoencoder,
-    context_encoder_variational_autoencoder_Zimmerer,
-    fanogan,
-    gaussian_mixture_variational_autoencoder,
-    gaussian_mixture_variational_autoencoder_spatial,
-    gaussian_mixture_variational_autoencoder_You,
-    variational_autoencoder,
-    variational_autoencoder_Zimmerer,
-)
+from models import autoencoder
 from trainers import Metrics
 from trainers.AE import AE
-from trainers.CE import CE
-from trainers.ceVAE import ceVAE
-
-# from trainers.fAnoGAN import fAnoGAN
-from trainers.ConstrainedAAE import ConstrainedAAE
-from trainers.ConstrainedAE import ConstrainedAE
-from trainers.GMVAE import GMVAE
-from trainers.GMVAE_spatial import GMVAE_spatial
-from trainers.VAE import VAE
-from trainers.VAE_You import VAE_You
 from utils import Evaluation
-from utils.brainweb_download import download_brainweb_dataset
 from utils.default_config_setup import Dataset, get_config, get_datasets, get_options
 from utils.Evaluation import (
     add_colorbar,
@@ -102,21 +71,42 @@ def recon(args):
     patient = dataset_pc.patients[1]
 
     patient = {
-        'name': os.path.basename(str(args.nii_filename)),
-        'type': "NORMAL",
-        'fullpath': str(args.nii_filename)
+        "name": os.path.basename(str(args.nii_filename)),
+        "type": "NORMAL",
+        "fullpath": str(args.nii_filename),
     }
-    patient['filtered_files'] = patient['fullpath']
+    patient["filtered_files"] = patient["fullpath"]
 
     patient["groundtruth_filename"] = "content/data/Brainweb/groundtruth/normal.nii.gz"
 
     nii_filename = patient["filtered_files"]
     output_dir = args.outdir
 
-    recon_eval(nii_filename, patient, datasetObj = dataset_hc, options=options, modelObj=modelObj, output_dir = output_dir)
+    recon_eval(
+        nii_filename,
+        patient,
+        datasetObj=dataset_hc,
+        options=options,
+        modelObj=modelObj,
+        output_dir=output_dir,
+        exportImages=args.exportImages,
+        exportVolume=args.exportVolume,
+    )
 
-def recon_eval(nii_filename, patient, datasetObj, options, modelObj, output_dir):
-    nii, nii_seg, nii_skullmap = datasetObj.load_volume_and_groundtruth(nii_filename, patient)
+
+def recon_eval(
+    nii_filename,
+    patient,
+    datasetObj,
+    options,
+    modelObj,
+    output_dir,
+    exportVolume,
+    exportImages,
+):
+    nii, nii_seg, nii_skullmap = datasetObj.load_volume_and_groundtruth(
+        nii_filename, patient
+    )
     p = 0
     sampleDir = output_dir
     prior_quantile = np.quantile(nii.data, 0.9)
@@ -168,7 +158,7 @@ def recon_eval(nii_filename, patient, datasetObj, options, modelObj, output_dir)
                 results = modelObj.reconstruct(x, dropout=True)
             else:
                 results = modelObj.reconstruct(x)
-            x_rec_tmp = results['reconstruction']
+            x_rec_tmp = results["reconstruction"]
             if "log_var" in results:
                 x_log_vars += [results["log_var"]]
 
@@ -191,12 +181,12 @@ def recon_eval(nii_filename, patient, datasetObj, options, modelObj, output_dir)
         x_recs_var_epistemic * (2 * np.expand_dims(np.expand_dims(slice_skullmap, axis=0), axis=-1) - 1)
         # values outside the brain are getting negative, while values on the brain stay the same
 
-        _eval_dict['reconstructionTimes'] += [time.time() - _tmp]
+        _eval_dict["reconstructionTimes"] += [time.time() - _tmp]
 
         # Get a sample without dropout
-        x_rec = results['reconstruction']
-        l1err = results['l1err']
-        l2err = results['l2err']
+        x_rec = results["reconstruction"]
+        l1err = results["l1err"]
+        l2err = results["l2err"]
         if num_samples > 1:
             x_rec = x_recs_mean
         if should(options, "keepOnlyPositiveResiduals"):
@@ -212,35 +202,37 @@ def recon_eval(nii_filename, patient, datasetObj, options, modelObj, output_dir)
         subvolume_idx += 1
 
         # Fill eval array
-        _eval_dict['x'] += [x]
+        _eval_dict["x"] += [x]
         if num_samples > 1:
             _eval_dict['epistemic_variance'] += [x_recs_var_epistemic]
         _eval_dict['reconstructions'] += [x_rec]
         _eval_dict['labelmaps'] += [np.squeeze(labelmaps)]
         _eval_dict['l1reconstructionErrors'] += [l1err]
         _eval_dict['l2reconstructionErrors'] += [l2err]
-        imwrite(os.path.join(sampleDir, '{}_{}.png'.format(p, s)), normalize_and_squeeze(x))
-        imwrite(os.path.join(sampleDir, '{}_{}_rec.png'.format(p, s)), normalize_and_squeeze(x_rec))
-        imwrite(os.path.join(sampleDir, '{}_{}_gt.png'.format(p, s)), normalize_and_squeeze(labelmaps))  # check if normalization is useful
-        imwrite(os.path.join(sampleDir, '{}_{}_diff.png'.format(p, s)), normalize_and_squeeze(x_diff))
-        imwrite(os.path.join(sampleDir, '{}_{}_rec_variance_combined.png'.format(p, s)),
-                np.squeeze(apply_colormap(x_recs_var, plt.cm.jet)))
-        if x_log_vars.size > 0:
-            imwrite(os.path.join(sampleDir, '{}_{}_logvar.png'.format(p, s)), normalize_and_squeeze(np.mean(x_log_vars, axis=0)))
+        if exportImages:
+            imwrite(os.path.join(sampleDir, '{}_{}.png'.format(p, s)), normalize_and_squeeze(x))
+            imwrite(os.path.join(sampleDir, '{}_{}_rec.png'.format(p, s)), normalize_and_squeeze(x_rec))
+            imwrite(os.path.join(sampleDir, '{}_{}_gt.png'.format(p, s)), normalize_and_squeeze(labelmaps))  # check if normalization is useful
+            imwrite(os.path.join(sampleDir, '{}_{}_diff.png'.format(p, s)), normalize_and_squeeze(x_diff))
+            imwrite(os.path.join(sampleDir, '{}_{}_rec_variance_combined.png'.format(p, s)),
+                    np.squeeze(apply_colormap(x_recs_var, plt.cm.jet)))
+            if x_log_vars.size > 0:
+                imwrite(os.path.join(sampleDir, '{}_{}_logvar.png'.format(p, s)), normalize_and_squeeze(np.mean(x_log_vars, axis=0)))
 
     if should(options, "medianFiltering"):
         subvolume = apply_3d_median_filter(subvolume)
 
     _eval_dict['diffs'] += [subvolume]
 
-    for s in range(datasetObj.options.sliceStart, min(datasetObj.options.sliceEnd, nii.num_slices_along_axis(datasetObj.options.axis))):
-        imwrite(os.path.join(sampleDir, '{}_{}_diff_filtered.png'.format(p, s)),
-                normalize_and_squeeze(subvolume[s - datasetObj.options.sliceStart]))
-        squashed = squash_intensities(np.squeeze(subvolume[s - datasetObj.options.sliceStart]))
-        squashed = add_colorbar(squashed)
-        imwrite(os.path.join(sampleDir, '{}_{}_heatmap.png'.format(p, s)), np.squeeze(apply_colormap(squashed, plt.cm.jet)))
+    if exportImages:
+        for s in range(datasetObj.options.sliceStart, min(datasetObj.options.sliceEnd, nii.num_slices_along_axis(datasetObj.options.axis))):
+            imwrite(os.path.join(sampleDir, '{}_{}_diff_filtered.png'.format(p, s)),
+                    normalize_and_squeeze(subvolume[s - datasetObj.options.sliceStart]))
+            squashed = squash_intensities(np.squeeze(subvolume[s - datasetObj.options.sliceStart]))
+            squashed = add_colorbar(squashed)
+            imwrite(os.path.join(sampleDir, '{}_{}_heatmap.png'.format(p, s)), np.squeeze(apply_colormap(squashed, plt.cm.jet)))
 
-    if should(options, "exportVolumes"):
+    if exportVolume:
         dezoom_factor = tuple([1]) + tuple(1 / np.asarray(zoom_factor))
         subvolume_deprocessed = scipy.ndimage.interpolation.zoom(subvolume, dezoom_factor)
         nii_seg.set_to_zero()
@@ -258,7 +250,7 @@ if __name__ == "__main__":
     args = argparse.ArgumentParser(description='Framework')
     args.add_argument("--nii", type=Path, help="Patient path", dest="nii_filename", required=True)
     args.add_argument("--outdir", type=Path, help="Output dir", required=True)
-    args.add_argument('-c', '--config', default='config.default.json', type=str, help='config-path')
+    args.add_argument('-c', '--config', default='config.json', type=str, help='config-path')
     args.add_argument('-b', '--batchsize', default=8, type=int, help='batchsize')
     args.add_argument('-l', '--lr', default=0.0001, type=float, help='learning rate')
     args.add_argument('-E', '--numEpochs', default=1000, type=int, help='how many epochs to train')
@@ -273,6 +265,10 @@ if __name__ == "__main__":
     args.add_argument('-m', '--model', default='autoencoder', type=str, help='Can be every class from models directory')
     args.add_argument('-O', '--threshold', default=None, type=float, help='Use predefined ThreshOld')
     args.add_argument('-d', '--ds', default=None, type=Dataset, help='Only evaluate on given dataset')
+
+    # export options
+    args.add_argument('--exportVolume', default=True, type=bool, action=argparse.BooleanOptionalAction, help='Export volume')
+    args.add_argument('--exportImages', default=False, type=bool, action=argparse.BooleanOptionalAction, help='Export images')
 
     # following arguments are only relevant for specific architectures
     args.add_argument('-n', '--numMonteCarloSamples', default=0, type=int, help='Amount of Monte Carlos Samples during restoration')
